310761a52dd425504e960f5edbe56a28f79ae098
Nicolas Drebenstedt <drebenstedtn@rki.de>
2023-11-16 17:36:04 +0100

    Package mex-model as a python distribution

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mex-model",
+    version="2.1.1",
+    author_email="mex@rki.de",
+    author="RKI MEx Team",
+    description="RKI MEx metadata model",
+    license="MIT",
+    package_dir={"mex.model": "schema"},
+)
