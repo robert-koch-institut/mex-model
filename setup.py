f9d35adc745fe118962b59ae1e6852d1440c6e3f
Nicolas Drebenstedt <drebenstedtn@rki.de>
2023-12-12 15:01:04 +0100

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="mex-model",
-    version="2.1.1",
+    version="2.2.0",
     author_email="mex@rki.de",
     author="RKI MEx Team",
     description="RKI MEx metadata model",
