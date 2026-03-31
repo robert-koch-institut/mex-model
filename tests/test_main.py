71c16755422957ffc85e770f72ab400cc9d0d292
RKI | Metadata Exchange <121876825+RKIMetadataExchange@users.noreply.github.com>
2026-03-31 18:19:55 +0200

    Bump cookiecutter template to 48ce42 (#420)
    ### Changes
    
    - bumped cookiecutter template to
    https://github.com/robert-koch-institut/mex-template/commit/48ce42
    
    ---------
    
    Co-authored-by: Nicolas Drebenstedt <drebenstedtn@rki.de>

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from mex.model import (
     ENTITY_JSON_BY_NAME,
     EXTRACTED_MODEL_JSON_BY_NAME,
@@ -6,10 +8,13 @@ from mex.model import (
     VOCABULARY_JSON_BY_NAME,
 )
 
+MODEL_ROOT = Path(__file__).resolve().parent.parent / "mex" / "model"
+
 
 def test_extracted_model_json_by_name() -> None:
     # sanity check on count
-    assert len(EXTRACTED_MODEL_JSON_BY_NAME) == 13
+    expected = len(list((MODEL_ROOT / "entities").glob("extracted-*.json")))
+    assert len(EXTRACTED_MODEL_JSON_BY_NAME) == expected
     # spot check on content
     assert (
         EXTRACTED_MODEL_JSON_BY_NAME["bibliographic_resource"]["properties"][
@@ -21,7 +26,8 @@ def test_extracted_model_json_by_name() -> None:
 
 def test_merged_model_json_by_name() -> None:
     # sanity check on count
-    assert len(MERGED_MODEL_JSON_BY_NAME) == 13
+    expected = len(list((MODEL_ROOT / "entities").glob("merged-*.json")))
+    assert len(MERGED_MODEL_JSON_BY_NAME) == expected
     # spot check on content
     assert (
         MERGED_MODEL_JSON_BY_NAME["primary_source"]["properties"]["unitInCharge"][
@@ -33,7 +39,8 @@ def test_merged_model_json_by_name() -> None:
 
 def test_field_json_by_name() -> None:
     # sanity check on count
-    assert len(FIELD_JSON_BY_NAME) == 3
+    expected = len(list((MODEL_ROOT / "fields").glob("*.json")))
+    assert len(FIELD_JSON_BY_NAME) == expected
     # spot check on content
     assert (
         FIELD_JSON_BY_NAME["link"]["properties"]["url"]["examples"][0]
@@ -43,7 +50,8 @@ def test_field_json_by_name() -> None:
 
 def test_vocabulary_json_by_name() -> None:
     # sanity check on count
-    assert len(VOCABULARY_JSON_BY_NAME) == 18
+    expected = len(list((MODEL_ROOT / "vocabularies").glob("*.json")))
+    assert len(VOCABULARY_JSON_BY_NAME) == expected
     # spot check on content
     assert (
         VOCABULARY_JSON_BY_NAME["api_type"][0]["identifier"]
